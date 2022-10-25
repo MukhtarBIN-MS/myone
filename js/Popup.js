6e2f999a071761e27f707715787a0b48c10a0c47
binms <myl772ng@gmail.com>
2022-10-25 12:56:08 +0100

    first commit

---
+++ js/Popup.js
@@ -0,0 +1,13 @@
+
+function myFunction() {
+    setTimeout(function () {
+        document.getElementById('popUpMain').style.display = "block"
+    }, 36489025000);
+}
+
+function dismiss() {
+    document.getElementById('popUpMain').style.display = "none";
+}
+
+
+
---
+++ js/get.js
@@ -0,0 +1,11 @@
+ <script>
+    if (!localStorage.bannerClosed){
+        $('.covid-banner').css('display', 'inherit');
+    }
+    else{
+        $('.covid-banner').css('display','none');
+    }
+
+   
+
+    </script>
